95220e0f997c5e800c18313d9e00f700f490ac64
EnderAgentX <artemer200@gmail.com>
2023-04-08 20:46:33 +0300

    fix

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	_ "github.com/denisenkom/go-mssqldb"
@@ -15,64 +14,26 @@ func main() {
 		log.Fatal("Ошибка при открытии соединения: ", err.Error())
 		return
 	}
-	ctx := context.Background()
-	err = db.PingContext(ctx)
-	count, err := ReadEmployees(db)
-	fmt.Printf("Read %d row(s) successfully.\n", count)
+	ReadPhone(db)
+	defer db.Close()
+}
 
-	var phone string
-	query := "SELECT Phone FROM Student"
+func ReadPhone(db *sql.DB) {
+	var id int
+	var name, phone string
+	count := 0
+	query := "SELECT Id, FullName, Phone FROM Student"
 	rows, err := db.Query(query)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for rows.Next() {
-		if err := rows.Scan(&phone); err != nil {
+		if err := rows.Scan(&id, &name, &phone); err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(phone)
-	}
-
-	defer rows.Close()
-	defer rows.Close()
-	defer db.Close()
-}
-
-func ReadEmployees(db *sql.DB) (int, error) {
-	ctx := context.Background()
-
-	// Check if database is alive.
-	err := db.PingContext(ctx)
-	if err != nil {
-		return -1, err
-	}
-
-	tsql := fmt.Sprintf("SELECT Id, FullName, Phone FROM Student;")
-
-	// Execute query
-	rows, err := db.QueryContext(ctx, tsql)
-	if err != nil {
-		return -1, err
-	}
-
-	defer rows.Close()
-
-	var count int
-
-	// Iterate through the result set.
-	for rows.Next() {
-		var name, phone string
-		var id int
-
-		// Get values from row.
-		err := rows.Scan(&id, &name, &phone)
-		if err != nil {
-			return -1, err
-		}
-
 		fmt.Printf("Id: %d, Name: %s, Phone: %s\n", id, name, phone)
 		count++
 	}
-
-	return count, nil
+	fmt.Printf("Read %d row(s) successfully.\n", count)
+	defer rows.Close()
 }
