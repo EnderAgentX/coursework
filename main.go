3c7197312ab36210e443c7fdfd5a09714c856e14
EnderAgentX <artemer200@gmail.com>
2023-04-08 20:40:48 +0300

    Test

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	_ "github.com/denisenkom/go-mssqldb"
@@ -14,11 +15,11 @@ func main() {
 		log.Fatal("Ошибка при открытии соединения: ", err.Error())
 		return
 	}
-	ReadPhone(db)
-	defer db.Close()
-}
+	ctx := context.Background()
+	err = db.PingContext(ctx)
+	count, err := ReadEmployees(db)
+	fmt.Printf("Read %d row(s) successfully.\n", count)
 
-func ReadPhone(db *sql.DB) {
 	var phone string
 	query := "SELECT Phone FROM Student"
 	rows, err := db.Query(query)
@@ -31,5 +32,47 @@ func ReadPhone(db *sql.DB) {
 		}
 		fmt.Println(phone)
 	}
+
 	defer rows.Close()
+	defer rows.Close()
+	defer db.Close()
+}
+
+func ReadEmployees(db *sql.DB) (int, error) {
+	ctx := context.Background()
+
+	// Check if database is alive.
+	err := db.PingContext(ctx)
+	if err != nil {
+		return -1, err
+	}
+
+	tsql := fmt.Sprintf("SELECT Id, FullName, Phone FROM Student;")
+
+	// Execute query
+	rows, err := db.QueryContext(ctx, tsql)
+	if err != nil {
+		return -1, err
+	}
+
+	defer rows.Close()
+
+	var count int
+
+	// Iterate through the result set.
+	for rows.Next() {
+		var name, phone string
+		var id int
+
+		// Get values from row.
+		err := rows.Scan(&id, &name, &phone)
+		if err != nil {
+			return -1, err
+		}
+
+		fmt.Printf("Id: %d, Name: %s, Phone: %s\n", id, name, phone)
+		count++
+	}
+
+	return count, nil
 }
